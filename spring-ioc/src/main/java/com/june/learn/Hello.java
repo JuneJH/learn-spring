6c7b785110ace1c4b92fa5d954aa1416d25a03f5
JuneJH <15310487388@163.com>
2023-03-08 00:06:41 +0800

    feat(*): spring-ioc

---
+++ spring-ioc/src/main/java/com/june/learn/Hello.java
@@ -0,0 +1,12 @@
+package com.june.learn;
+
+public class Hello {
+    private String name;
+    public void sayHello(){
+        System.out.println("hello"+this.name);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ spring-ioc/src/test/java/Test.java
@@ -0,0 +1,15 @@
+import com.june.learn.Hello;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        // new 方式获取
+        Hello h1 = new Hello();
+        h1.sayHello();
+        // spring 方式获取
+        ApplicationContext app = new ClassPathXmlApplicationContext("bean.xml");
+        Hello h2 = (Hello) app.getBean("hello");
+        h2.sayHello();
+    }
+}
