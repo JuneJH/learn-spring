641cc819e8cf59a898f31ea2871fd0698b5cb22f
JuneJH <15310487388@163.com>
2023-03-09 23:59:13 +0800

    feat(*): spring-aop 自定义类

---
+++ spring-aop/src/main/java/com/june/common/Hook.java
@@ -0,0 +1,12 @@
+package com.june.common;
+
+public class Hook {
+
+    public void before(){
+        System.out.println("=============执行之前===========");
+    }
+
+    public void after(){
+        System.out.println("============执行之后=============");
+    }
+}
