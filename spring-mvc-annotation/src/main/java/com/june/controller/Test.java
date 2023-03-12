1bcd8f569953489878078625cd8735f95e452326
JuneJH <15310487388@163.com>
2023-03-12 16:51:27 +0800

    feat(*): hello spring-mvc-annotation

---
+++ spring-mvc-annotation/src/main/java/com/june/controller/Test.java
@@ -0,0 +1,17 @@
+package com.june.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Test {
+    public Test() {
+        System.out.println("创建了");
+    }
+
+    @RequestMapping("/test")
+    public String Test(){
+        System.out.println("调用了");
+        return  "test";
+    }
+}
---
+++ spring-mvc-annotation/web/WEB-INF/jsp/test.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: june
+  Date: 2023/3/12
+  Time: 11:47
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>测试注解方式</title>
+</head>
+<body>
+hello annotation！！！！
+</body>
+</html>
